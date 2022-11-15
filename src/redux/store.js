c4be2fad4f82f99d76f9fc5c1a8b30b8f143c570
Kateryna Kleshch <ketrins1993@gmail.com>
2022-11-15 21:18:58 +0200

    install redux and devTools and add files

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enhancer);
