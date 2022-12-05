7641db92ef79cae73b545b90442fab9c22306d59
Kateryna Kleshch <ketrins1993@gmail.com>
2022-12-05 19:29:53 +0200

    add auth initial state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import { authReducer } from './user/authSlice';
 
 const reducers = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
+  auth: authReducer,
 });
 
 export const store = configureStore({
